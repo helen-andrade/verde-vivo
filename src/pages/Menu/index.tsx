6adb3b77019dac744662bbb9a7f9f1a7264f6b28
Seu Nome <seuemail@exemplo.com>
2025-02-12 15:50:38 -0300

    feat: melhora a legibilidade do código do menu

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -7,18 +7,33 @@ import img2 from "../../assets/saladaProteica/02.jpg";
 import img3 from "../../assets/saladaProteica/03.jpg";
 import img4 from "../../assets/saladaProteica/04.jpg";
 import img5 from "../../assets/saladaProteica/05.jpg";
-import "../../../public/icons/bag.svg";
 
 const Menu = () => {
   const [menuModal, setMenuModal] = useState(false);
 
   const openMenuModal = () => {
     setMenuModal(true);
+    document.body.classList.add("modal-open");
   };
 
   const closeMenuModal = () => {
     setMenuModal(false);
+    document.body.classList.remove("modal-open");
   };
+
+  const dishes = [
+    { id: 1, img: img1, price: "R$27,90" },
+    { id: 2, img: img2, price: "R$12,90" },
+    { id: 3, img: img3, price: "R$17,90" },
+    { id: 4, img: img4, price: "R$25,00" },
+    { id: 5, img: img5, price: "R$32,50" },
+    { id: 6, img: img1, price: "R$27,90" },
+    { id: 7, img: img2, price: "R$12,90" },
+    { id: 8, img: img3, price: "R$17,90" },
+    { id: 9, img: img4, price: "R$25,00" },
+    { id: 10, img: img5, price: "R$32,50" },
+  ];
+
   return (
     <div>
       <Navbar />
@@ -34,10 +49,7 @@ const Menu = () => {
       <div className="menuCategories">
         <div className="categoriesFoods">
           <button onClick={openMenuModal}>
-            <a href="#">Saladas proteicas</a>
-          </button>
-          <button onClick={openMenuModal}>
-            <a href="#">Saladas +pedidas</a>
+            <a href="#">Saladas</a>
           </button>
           <button onClick={openMenuModal}>
             <a href="#">Proteínas</a>
@@ -53,27 +65,38 @@ const Menu = () => {
 
       {menuModal && (
         <div className="modalOverlay">
-          <div className="modalContent">
+          <div className="modalContentMenu">
             <button className="closeButton" onClick={closeMenuModal}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                id="Layer_1"
-                data-name="Layer 1"
-                viewBox="0 0 24 24"
                 width="35"
                 height="35"
+                viewBox="0 0 24 24"
               >
-                <path d="m16.535,8.172l-3.828,3.828,3.828,3.828-.707.707-3.828-3.828-3.828,3.828-.707-.707,3.828-3.828-3.828-3.828.707-.707,3.828,3.828,3.828-3.828.707.707Zm7.465,3.828c0,6.617-5.383,12-12,12S0,18.617,0,12,5.383,0,12,0s12,5.383,12,12Zm-1,0c0-6.065-4.935-11-11-11S1,5.935,1,12s4.935,11,11,11,11-4.935,11-11Z" />
+                <path d="m16.535,8.172l-3.828,3.828 3.828,3.828-.707.707-3.828-3.828-3.828,3.828-.707-.707,3.828-3.828-3.828-3.828.707-.707,3.828,3.828,3.828-3.828.707.707Zm7.465,3.828c0,6.617-5.383,12-12,12S0,18.617,0,12,5.383,0,12,0s12,5.383,12,12Zm-1,0c0-6.065-4.935-11-11-11S1,5.935,1,12s4.935,11,11,11,11-4.935,11-11Z" />
               </svg>
             </button>
-            <div>
-              <div className="imgMenu">
-                <img src={img1} alt="Imagem 1" />
-                <img src={img2} alt="Imagem 2" />
-                <img src={img3} alt="Imagem 3" />
-                <img src={img4} alt="Imagem 4" />
-                <img src={img5} alt="Imagem 5" />
-              </div>
+
+            <div className="imgMenu">
+              {dishes.map((dish) => (
+                <div key={dish.id} className="imgDiv">
+                  <img src={dish.img} alt={`Imagem ${dish.id}`} />
+                  <p className="valueDish">
+                    {dish.price}
+                    <svg
+                      className="svgIcon"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="#4CAF50"
+                    >
+                      <title>Carrinho de Compras</title>
+                      <path d="M7 18c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm10 0c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zM7.334 13l.805 2H19a1 1 0 0 0 .928-.629l2.9-7.27A.999.999 0 0 0 21.999 6H5.756L4.896 3.683A1 1 0 0 0 3.956 3H1v2h2.365l3.6 9H7.334z" />
+                    </svg>
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
